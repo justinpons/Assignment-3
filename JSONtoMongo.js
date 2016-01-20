e5b081c8427ff563a02feb112a51e274e3b42e2e
justintime5046 <justintime5046@gmail.com>
2016-01-19 20:29:04 -0500

    Added functions

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -10,12 +10,31 @@ var fs = require('fs'),
     config = require('./config');
 
 /* Connect to your database */
+mongoose.connect(config.db);
 
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
   and then save it to your Mongo database 
  */
+fs.readFileAsync('listings.json', 'utf-8').then(function(resolve, reject) {
+  var listings = JSON.parse(resolve);
 
+  listings.forEach (function(listing) {
+    var newListing = new Listing({
+      code: listing.code,
+      name: listing.name,
+      coordinated: {
+        latitude: listing.coordinate.latitude,
+        longitude: listing.coordinates.longitude
+      },
+      address: listing.address,
+    });
+
+    newListing.save(function(eff, s){
+      console.log(s);
+    });
+  });
+});
 
 /* 
   Once you've written + run the script, check out your MongoLab database to ensure that 
--- ListingSchema.js
+++ ListingSchema.js
@@ -5,11 +5,33 @@ var mongoose = require('mongoose'),
 /* Create your schema */
 var listingSchema = new Schema({
   /* your code here */
+  code: String,
+  name: String,
+  coordinates: { 
+  	latitude: Number, 
+  	longitude: Number
+  },
+  address: String,
+  created_at: Date,
+  updated_at: Date,
 });
 
 /* create a 'pre' function that adds the updated_at (and created_at if not already there) property */
 listingSchema.pre('save', function(next) {
   /* your code here */
+  
+  // get the current date
+  var currentDate = new Date();
+  
+  // change the updated_at field to current date
+  this.updated_at = currentDate;
+
+  // if created_at does not exist, add to that field
+  if(!this.created_at) {
+  	this.created_at = currentDate;
+  }
+
+  next();
 });
 
 /* Use your schema to instantiate a Mongoose model */
